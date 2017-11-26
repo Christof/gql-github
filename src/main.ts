804e0885ece3f4a02953cc40ff36d9945a3a0928
christof <christof.sirk@gmail.com>
2017-11-26 17:03:16 +0100

    Add token for authentication and fix TS errors.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import fetch from 'node-fetch';
-import gql from 'graphql-tag';
-import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import fetch from "node-fetch";
+import gql from "graphql-tag";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+const authLink = setContext((_, { headers }) => {
+  const token = process.env.TOKEN;
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null
+    }
+  };
+});
 
 const lastReleaseQuery = gql(`
 query {
@@ -20,13 +31,18 @@ query {
     }
   }
 }
-`)
+`);
 
-const client = new ApolloClient({
-  link: createHttpLink({ uri: 'https://api.github.com/graphql', fetch }),
-  cache: new InMemoryCache(),
+const httpLink = createHttpLink({
+  uri: "https://api.github.com/graphql",
+  fetch: fetch as any
 });
 
-client.query(lastReleaseQuery).then((data: any) => {
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
+client.query({ query: lastReleaseQuery }).then((data: any) => {
   console.log(data.data);
-})
+});
