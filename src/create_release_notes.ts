2506355a6d9f35dab68fcf95190f8b07299646b7
christof <christof.sirk@gmail.com>
2017-11-29 20:50:34 +0100

    Execute git command to get merge commits of PRs between tags.

---
+++ src/create_release_notes.ts
@@ -0,0 +1,36 @@
+import * as program from "commander";
+import { promisify } from "util";
+import { exec } from "child_process";
+const sh = promisify(exec);
+
+program
+  .version("0.0.1")
+  .option("--owner <repository owner>", "Owner of the repository")
+  .option("--repo <repository name>", "Name of the repository")
+  .option("--start <tag>", "The start tag of the range for the release")
+  .option("--end <tag>", "The end tag of the range for the release")
+  .parse(process.argv);
+
+const repositoryOwner = program.owner;
+const repositoryName = program.repo;
+
+const start = program.start;
+const end = program.end;
+
+async function getMergeCommitsBetweenTags(
+  start: string,
+  end: string,
+  repo: string
+) {
+  const { stdout, stderr } = await sh(
+    `cd ~/Documents/${repo} && \
+    git log ${start}..${end} | \
+    grep "Merge pull" --after-context=2`
+  );
+
+  if (stderr) throw new Error(stderr);
+
+  console.log(stdout);
+}
+
+getMergeCommitsBetweenTags(start, end, repositoryName);
