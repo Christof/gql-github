657b2e80bf0dd879c5277d67b34c3b0952fedeec
christof <christof.sirk@gmail.com>
2017-11-30 21:02:23 +0100

    Move functions into new ReleaseNoteCreator class.

--- src/create_release_notes.ts
+++ src/create_release_notes.ts
@@ -18,97 +18,102 @@ const repositoryName = program.repo;
 const start = program.start;
 const end = program.end;
 
-async function getMergeCommitsBetweenTags(
-  start: string,
-  end: string,
-  repo: string
-) {
-  const { stdout, stderr } = await sh(
-    `cd ~/Documents/${repo} && \
+class ReleaseNoteCreator {
+  async getMergeCommitsBetweenTags(start: string, end: string, repo: string) {
+    const { stdout, stderr } = await sh(
+      `cd ~/Documents/${repo} && \
     git log ${start}..${end} | \
     grep "Merge pull" --after-context=2`
-  );
+    );
 
-  if (stderr) throw new Error(stderr);
+    if (stderr) throw new Error(stderr);
 
-  const lines = stdout.split("\n");
-  const importantLines = lines.reduce(
-    (accumulator, line) => {
-      if (/\S/.test(line) && line.indexOf("--") !== 0) accumulator.push(line);
+    const lines = stdout.split("\n");
+    const importantLines = lines.reduce(
+      (accumulator, line) => {
+        if (/\S/.test(line) && line.indexOf("--") !== 0) accumulator.push(line);
 
-      return accumulator;
-    },
-    [] as string[]
-  );
+        return accumulator;
+      },
+      [] as string[]
+    );
 
-  const pullRequestNumberRegex = new RegExp(/#\d*/);
-  const pullRequests = importantLines.reduce(
-    (accumulator, line, index, array) => {
-      if (index % 2 === 0) return accumulator;
+    const pullRequestNumberRegex = new RegExp(/#\d*/);
+    const pullRequests = importantLines.reduce(
+      (accumulator, line, index, array) => {
+        if (index % 2 === 0) return accumulator;
 
-      const pullRequestNumberMatch = array[index - 1].match(
-        pullRequestNumberRegex
-      );
-      const result = `- ${line.trim()} (${pullRequestNumberMatch})`;
-      accumulator.push(result);
-      return accumulator;
-    },
-    [] as string[]
-  );
+        const pullRequestNumberMatch = array[index - 1].match(
+          pullRequestNumberRegex
+        );
+        const result = `- ${line.trim()} (${pullRequestNumberMatch})`;
+        accumulator.push(result);
+        return accumulator;
+      },
+      [] as string[]
+    );
 
-  return pullRequests;
-}
+    return pullRequests;
+  }
+  async assignPRsToCategory(pullRequests: string[]) {
+    const basicChanges = [] as string[];
+    const trainingChanges = [] as string[];
+    const breakingChanges = [] as string[];
 
-async function assignPRsToCategory(pullRequests: string[]) {
-  const basicChanges = [] as string[];
-  const trainingChanges = [] as string[];
-  const breakingChanges = [] as string[];
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
 
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+    console.log("Assign PRs to category:");
+    console.log("  b -> breaking change");
+    console.log("  t -> training change");
+    console.log("default is basic change");
+    const questions = pullRequests.map(pullRequest => {
+      return () => {
+        return new Promise(resolve => {
+          rl.question(`'${pullRequest} [b, t]?`, answer => {
+            if (answer === "b") {
+              breakingChanges.push(pullRequest);
+            } else if (answer === "t") {
+              trainingChanges.push(pullRequest);
+            } else {
+              basicChanges.push(pullRequest);
+            }
 
-  console.log("Assign PRs to category:");
-  console.log("  b -> breaking change");
-  console.log("  t -> training change");
-  console.log("default is basic change");
-  const questions = pullRequests.map(pullRequest => {
-    return () => {
-      return new Promise(resolve => {
-        rl.question(`'${pullRequest} [b, t]?`, answer => {
-          if (answer === "b") {
-            breakingChanges.push(pullRequest);
-          } else if (answer === "t") {
-            trainingChanges.push(pullRequest);
-          } else {
-            basicChanges.push(pullRequest);
-          }
-
-          resolve();
+            resolve();
+          });
         });
-      });
-    };
-  });
+      };
+    });
 
-  for (const question of questions) await question();
+    for (const question of questions) await question();
 
-  console.log("\n\n---------- RELEASE NOTES ----------\n");
-  if (breakingChanges.length) {
-    console.log(
-      "**Breaking changes:**\n\n" + breakingChanges.join("\n") + "\n"
-    );
+    console.log("\n\n---------- RELEASE NOTES ----------\n");
+    if (breakingChanges.length) {
+      console.log(
+        "**Breaking changes:**\n\n" + breakingChanges.join("\n") + "\n"
+      );
+    }
+    if (basicChanges.length) {
+      console.log("**Basic changes:**\n\n" + basicChanges.join("\n") + "\n");
+    }
+    if (trainingChanges.length) {
+      console.log(
+        "**Training changes:**\n\n" + trainingChanges.join("\n") + "\n"
+      );
+    }
   }
-  if (basicChanges.length) {
-    console.log("**Basic changes:**\n\n" + basicChanges.join("\n") + "\n");
-  }
-  if (trainingChanges.length) {
-    console.log(
-      "**Training changes:**\n\n" + trainingChanges.join("\n") + "\n"
+
+  async create(start: string, end: string, repo: string) {
+    const pullRequests = await this.getMergeCommitsBetweenTags(
+      start,
+      end,
+      repo
     );
+    await this.assignPRsToCategory(pullRequests);
   }
 }
 
-getMergeCommitsBetweenTags(start, end, repositoryName).then(pullRequests =>
-  assignPRsToCategory(pullRequests)
-);
+const releaseNoteCreator = new ReleaseNoteCreator();
+releaseNoteCreator.create(start, end, repositoryName);
