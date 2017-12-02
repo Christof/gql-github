589731dd6c0029670fbea18aa4b907d41b5bfbbf
christof <christof.sirk@gmail.com>
2017-12-02 20:55:38 +0100

    Extract Category into own file.

--- src/create_release_notes.ts
+++ src/create_release_notes.ts
@@ -1,3 +1,4 @@
+import { Category } from "./category";
 import * as program from "commander";
 import * as readline from "readline";
 import { promisify } from "util";
@@ -20,38 +21,6 @@ const start = program.start;
 const end = program.end;
 
 type QuestionCallback = () => Promise<{}>;
-
-class Category {
-  public pullRequests = [] as string[];
-
-  constructor(public header: string, public shortcut?: string) {}
-
-  match(input: string) {
-    return this.shortcut === undefined ? true : this.shortcut === input;
-  }
-
-  addIfMatching(input: string, pullRequest: string) {
-    if (this.match(input)) {
-      this.pullRequests.push(pullRequest);
-      return true;
-    }
-
-    return false;
-  }
-
-  printLegendLine() {
-    const key =
-      this.shortcut === undefined ? "default is " : `${this.shortcut} -> `;
-    console.log(key + this.header);
-  }
-
-  toString() {
-    if (this.pullRequests.length === 0) return "";
-
-    const heading = this.header.charAt(0).toUpperCase() + this.header.slice(1);
-    return `**${heading}:**\n\n${this.pullRequests.join("\n")}\n`;
-  }
-}
 
 class ReleaseNoteCreator {
   private categories = [
