daa39cacde7607fcadd33a9c8560f8f3a8ed5c8e
christof <christof.sirk@gmail.com>
2017-12-02 21:08:44 +0100

    Move CommitsFromRepository into own file.

--- src/release_note_creator.ts
+++ src/release_note_creator.ts
@@ -1,68 +1,10 @@
 import { Category } from "./category";
 import * as readline from "readline";
-import { promisify } from "util";
-import { exec } from "child_process";
 import * as request from "request-promise-native";
-const sh = promisify(exec);
+import { CommitsFromRepository } from "./commits_from_repository";
 
 type QuestionCallback = () => Promise<{}>;
 
-class CommitsFromRepository {
-  constructor(private repo: string) {}
-  async getCommitsBetweenTags(start: string, end: string) {
-    const { stdout, stderr } = await sh(
-      `cd ~/Documents/${this.repo} && \
-    git log ${start}..${end} | \
-    grep "Merge pull" --after-context=2`
-    );
-
-    if (stderr) throw new Error(stderr);
-
-    return stdout.split("\n");
-  }
-
-  private noneWhitespaceLinesReduce(accumulator: string[], line: string) {
-    if (/\S/.test(line) && line.indexOf("--") !== 0) accumulator.push(line);
-
-    return accumulator;
-  }
-
-  private pullRequestCommitsReduce(
-    accumulator: string[],
-    line: string,
-    index: number,
-    array: string[]
-  ) {
-    const pullRequestNumberRegex = new RegExp(/#\d*/);
-    if (index % 2 === 0) return accumulator;
-
-    const pullRequestNumberMatch = array[index - 1].match(
-      pullRequestNumberRegex
-    );
-    const result = `- ${line.trim()} (${pullRequestNumberMatch})`;
-    accumulator.push(result);
-    return accumulator;
-  }
-
-  async filterPullRequestCommits(lines: string[]) {
-    const importantLines = lines.reduce(
-      this.noneWhitespaceLinesReduce,
-      [] as string[]
-    );
-
-    const pullRequests = importantLines.reduce(
-      this.pullRequestCommitsReduce,
-      [] as string[]
-    );
-
-    return pullRequests;
-  }
-
-  async getPullRequestCommitsBetween(start: string, end: string) {
-    const commits = await this.getCommitsBetweenTags(start, end);
-    return await this.filterPullRequestCommits(commits);
-  }
-}
 export class ReleaseNoteCreator {
   private categories = [
     new Category("breaking changes", "b"),
