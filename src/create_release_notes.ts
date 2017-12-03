82b51b131a5e8b870574c861ef94a38bb1334d54
christof <christof.sirk@gmail.com>
2017-12-03 17:25:49 +0100

    Add program description to help.

--- src/create_release_notes.ts
+++ src/create_release_notes.ts
@@ -7,6 +7,13 @@ program
   .option("--repo <repository name>", "Name of the repository")
   .option("--start <tag>", "The start tag of the range for the release")
   .option("--end <tag>", "The end tag of the range for the release")
+  .description(
+    `Utility to get pull requests between start and end tags.
+  Therefore it requires the repository to be checked out at
+  '~/Documents/{repository name}/'. These PRs are categorized
+  by the user and a release description is created, which
+  is uploaded to github.`
+  )
   .parse(process.argv);
 
 const token = process.env.TOKEN;
