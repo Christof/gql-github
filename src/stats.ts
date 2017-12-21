6eee71eeb2f0b72183c97578965508824f44ac6c
christof <christof.sirk@gmail.com>
2017-12-21 21:12:07 +0100

    Change getCommitsPerAuthorSince to getCommitsPerAuthorInDateRange.

--- src/stats.ts
+++ src/stats.ts
@@ -73,11 +73,16 @@ function getCommitsPerAuthor(data: any[]) {
   }, {});
 }
 
-function getCommitsPerAuthorSince(data: any[], startTime: Date) {
-  const unixTime = Math.round(startTime.getTime() / 1000);
+function getCommitsPerAuthorInDateRange(
+  data: any[],
+  startTime: Date,
+  endTime = new Date()
+) {
+  const unixStartTime = Math.round(startTime.getTime() / 1000);
+  const unixEndTime = Math.round(endTime.getTime() / 1000);
   return data.reduce((acc, userEntry) => {
     const weeksInRange = userEntry.weeks.filter(
-      (week: any) => week.w > unixTime
+      (week: any) => week.w > unixStartTime && week.w <= unixEndTime
     );
     const commits = weeksInRange.reduce(
       (sum: number, week: any) => sum + week.c,
