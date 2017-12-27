734e7d529067e215f20dab704d1f178eb0878dd5
christof <christof.sirk@gmail.com>
2017-12-27 21:35:11 +0100

    Add text input for token and load repos on submit click.

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -1,56 +1,81 @@
 import * as React from "react";
+import { FormEvent, ChangeEvent } from "react";
 
 export interface HelloProps {
   compiler: string;
   framework: string;
 }
 
-// 'HelloProps' describes the shape of props.
-// State is never set so we use the '{}' type.
-export class Hello extends React.Component<HelloProps, {}> {
+interface State {
+  error: any;
+  token: string;
+  items: any[];
+}
+
+export class Hello extends React.Component<HelloProps, State> {
   constructor(props: HelloProps) {
     super(props);
     this.state = {
       error: null,
-      isLoaded: false,
+      token: "",
       items: []
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  async componentDidMount() {
+
+  async loadRepos() {
     try {
       const organisation = "skillslab";
-      const token = "TOKEN";
       const uri = `https://api.github.com/orgs/${organisation}/repos`;
       const params: RequestInit = {
         method: "GET",
         mode: "cors",
         headers: [
           ["User-Agent", organisation],
-          ["Authorization", `token ${token}`]
+          ["Authorization", `token ${this.state.token}`]
         ]
       };
       // auth: { bearer: token },
       // json: true
       const res = await fetch(uri, params);
-      const result = res.json();
+      const result = await res.json();
       console.log(result);
       this.setState({
-        isLoaded: true,
         items: result
       });
     } catch (error) {
       console.log(error);
       this.setState({
-        isLoaded: true,
         error
       });
     }
   }
+
+  handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    this.loadRepos();
+  }
+
+  handleChange(event: ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      token: event.target.value
+    });
+  }
   render() {
     return (
-      <h1>
-        Hello from {this.props.compiler} and {this.props.framework}!
-      </h1>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={this.state.token}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
