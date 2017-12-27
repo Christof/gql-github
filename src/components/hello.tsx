ccec6b99af1495250a5f31821dd976c4b3af0187
christof <christof.sirk@gmail.com>
2017-12-27 18:05:02 +0100

    Try to retrieve repositories. Just public for now.

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -8,6 +8,40 @@ export interface HelloProps {
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the '{}' type.
 export class Hello extends React.Component<HelloProps, {}> {
+  constructor(props: HelloProps) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+  async componentDidMount() {
+    try {
+      const organisation = "skillslab";
+      const uri = `https://api.github.com/orgs/${organisation}/repos`;
+      const params: RequestInit = {
+        method: "GET",
+        mode: "cors",
+        headers: [["User-Agent", organisation]]
+      };
+      // auth: { bearer: token },
+      // json: true
+      const res = await fetch(uri, params);
+      const result = res.json();
+      console.log(result);
+      this.setState({
+        isLoaded: true,
+        items: result
+      });
+    } catch (error) {
+      console.log(error);
+      this.setState({
+        isLoaded: true,
+        error
+      });
+    }
+  }
   render() {
     return (
       <h1>
