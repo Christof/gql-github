36df8a247a4114d7090a04c761edd0ca44c58dc8
christof <christof.sirk@gmail.com>
2017-12-29 17:29:50 +0100

    Send state along to github and check it on callback.
    This should prevent CSRF.

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -7,16 +7,20 @@ import { GithubCallback } from "./github_callback";
 import * as uuid from "node-uuid";
 
 export class App extends React.Component<{}, {}> {
+  constructor(props: any) {
+    super(props);
+    if (window.localStorage.githubState === undefined) {
+      window.localStorage.githubState = uuid.v4();
+    }
+  }
   render() {
-    const state = uuid.v4();
-    window.localStorage.state = state;
     const githubLoginUrl =
       "https://github.com/login/oauth/authorize?" +
       qs.stringify({
         client_id: "1e031c3e419938e53c8e",
         redirect_uri: window.location.origin + "/auth-callback",
         scope: "repo,user",
-        state
+        state: window.localStorage.githubState
       });
     return (
       <BrowserRouter>
--- src/components/github_callback.tsx
+++ src/components/github_callback.tsx
@@ -2,10 +2,17 @@ import * as React from "react";
 import * as qs from "qs";
 
 export class GithubCallback extends React.Component<{}, {}> {
+  code: string;
   constructor(props: { location: { search: string } }) {
     super(props);
     const queryPrams = qs.parse(props.location.search.substr(1));
-    console.log("code", queryPrams.code);
+    this.code = queryPrams.code;
+    const githubState = queryPrams.state;
+    if (githubState !== window.localStorage.githubState) {
+      throw new Error("Retrieved state not same sent one. Possible CSRF!");
+    }
+    delete window.localStorage.githubState;
+    console.log("code", this.code);
   }
 
   render() {
