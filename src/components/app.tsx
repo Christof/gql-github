dbece8f1be9068b365fb3ce960a2ab021f7014bb
christof <christof.sirk@gmail.com>
2017-12-29 17:08:40 +0100

    Use node-uuid to generate random state to pass to github.
    Also save it in local storage.

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -4,15 +4,19 @@ import * as qs from "qs";
 
 import { Hello } from "./hello";
 import { GithubCallback } from "./github_callback";
+import * as uuid from "node-uuid";
 
 export class App extends React.Component<{}, {}> {
   render() {
+    const state = uuid.v4();
+    window.localStorage.state = state;
     const githubLoginUrl =
       "https://github.com/login/oauth/authorize?" +
       qs.stringify({
         client_id: "1e031c3e419938e53c8e",
         redirect_uri: window.location.origin + "/auth-callback",
-        scope: "repo,user"
+        scope: "repo,user",
+        state
       });
     return (
       <BrowserRouter>
