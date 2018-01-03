22f5554a83e9771fdff0ac636bc7f5dd0c1dad05
christof <christof.sirk@gmail.com>
2018-01-03 20:34:21 +0100

    Start to add signout functionality.

--- src/components/github_button.tsx
+++ src/components/github_button.tsx
@@ -7,10 +7,12 @@ export class GithubButton extends React.Component<Props, {}> {
   constructor(props: Props) {
     super(props);
 
-    this.githubInteraction = this.githubInteraction.bind(this);
+    this.signout = this.signout.bind(this);
   }
 
-  githubInteraction() {}
+  signout() {
+    window.localStorage.clear();
+  }
 
   render() {
     const githubLoginUrl =
@@ -21,6 +23,17 @@ export class GithubButton extends React.Component<Props, {}> {
         scope: "repo,user",
         state: window.localStorage.githubState
       });
+
+    if (window.localStorage.github !== undefined) {
+      return (
+        <button
+          className="f6 link dim ba ph3 pv2 mh2 mb2 dib bg-light-blue"
+          onClick={this.signout}
+        >
+          Signout from GitHub
+        </button>
+      );
+    }
     return (
       <a
         className="f6 link dim ba ph3 pv2 mh2 mb2 dib bg-light-blue"
