d2b5eaee415957d14678ad67037c0cce9942fa58
christof <christof.sirk@gmail.com>
2018-01-08 22:12:30 +0100

    Add default disabled option to Dropdown.

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -8,7 +8,13 @@ export interface Props {
 export class Dropdown extends React.Component<Props, {}> {
   render() {
     return (
-      <select onChange={event => this.props.onSelect(event.target.value)}>
+      <select
+        onChange={event => this.props.onSelect(event.target.value)}
+        defaultValue="none"
+      >
+        <option disabled value="none">
+          Select another item
+        </option>
         {this.props.options.map(option => (
           <option key={option} value={option}>
             {option}
