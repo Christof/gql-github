98c561abaf7b381c7bc02a1c360f3d77bb117f42
christof <christof.sirk@gmail.com>
2018-01-16 19:28:37 +0100

    Add another test case for retrieving repositories from user.

--- __tests__/github_test.ts
+++ __tests__/github_test.ts
@@ -20,5 +20,24 @@ describe("Github", () => {
         "https://api.github.com/orgs/owner/repos"
       );
     });
+
+    it("if first fails requests repositories from user named owner", async () => {
+      fetchMock.mockReset();
+      fetchMock.mockReturnValueOnce({
+        status: 404
+      });
+      fetchMock.mockReturnValueOnce({
+        status: 200,
+        json() {
+          return [{ name: "repository" }];
+        }
+      });
+      await github.getRepositories();
+
+      expect(fetchMock.mock.calls.length).toBe(2);
+      expect(fetchMock.mock.calls[1][0]).toBe(
+        "https://api.github.com/users/owner/repos"
+      );
+    });
   });
 });
