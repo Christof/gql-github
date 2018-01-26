0612e92a2d538bd7a9a77fa978284f1a2d0d98f1
christof <christof.sirk@gmail.com>
2018-01-26 20:25:56 +0100

    Add first draft of repository selection with checkboxes.

--- src/components/detailed_repository_selector.tsx
+++ src/components/detailed_repository_selector.tsx
@@ -17,6 +17,7 @@ interface Props {
 interface State {
   owners: string[];
   ownersState: boolean[];
+  repositories: { name: string; selected: boolean }[][];
 }
 
 export class DetailedRepositorySelector extends React.Component<Props, State> {
@@ -24,7 +25,8 @@ export class DetailedRepositorySelector extends React.Component<Props, State> {
     super(props);
     this.state = {
       owners: [],
-      ownersState: []
+      ownersState: [],
+      repositories: []
     };
 
     props.github.getOwners().then(owners => {
@@ -33,11 +35,33 @@ export class DetailedRepositorySelector extends React.Component<Props, State> {
     });
   }
 
+  createDefaultRepositoryState(repository: string) {
+    return { name: repository, selected: true };
+  }
+
+  async loadRepositoriesFromOwners() {
+    const repositories = await Promise.all(
+      this.state.owners.map(async (owner, index) => {
+        if (!this.state.ownersState[index]) return [];
+
+        const names = await this.props.github
+          .copyFor(owner)
+          .getRepositoryNames();
+
+        return names.map(repository =>
+          this.createDefaultRepositoryState(repository)
+        );
+      })
+    );
+
+    this.setState({ repositories });
+  }
+
   renderOwnerCheckbox(owner: string, index: number) {
     const handleChange = (_event: any, checked: boolean) => {
       const ownersState = [...this.state.ownersState];
       ownersState[index] = checked;
-      this.setState({ ownersState });
+      this.setState({ ownersState }, () => this.loadRepositoriesFromOwners());
     };
 
     return (
@@ -55,6 +79,36 @@ export class DetailedRepositorySelector extends React.Component<Props, State> {
     );
   }
 
+  renderRepositoryCheckbox(
+    ownerIndex: number,
+    name: string,
+    selected: boolean
+  ) {
+    const handleChange = (_event: any, checked: boolean) => {
+      const ownersRepos = [...this.state.repositories[ownerIndex]];
+      ownersRepos.find(repoState => repoState.name === name).selected = checked;
+
+      const repositories = [...this.state.repositories];
+      repositories[ownerIndex] = ownersRepos;
+
+      this.setState({ repositories });
+    };
+
+    return (
+      <FormControlLabel
+        key={`checked-${name}`}
+        control={
+          <Checkbox
+            checked={selected}
+            onChange={handleChange}
+            value={`checked-${name}`}
+          />
+        }
+        label={name}
+      />
+    );
+  }
+
   render() {
     return (
       <Section>
@@ -66,6 +120,21 @@ export class DetailedRepositorySelector extends React.Component<Props, State> {
           <FormGroup row>
             {this.state.owners.map((owner, index) =>
               this.renderOwnerCheckbox(owner, index)
+            )}
+          </FormGroup>
+        </FormControl>
+
+        <FormControl component="fieldset">
+          <FormLabel component="legend">Repositories</FormLabel>
+          <FormGroup row>
+            {this.state.repositories.map((ownerRepositories, ownerIndex) =>
+              ownerRepositories.map(repository =>
+                this.renderRepositoryCheckbox(
+                  ownerIndex,
+                  repository.name,
+                  repository.selected
+                )
+              )
             )}
           </FormGroup>
         </FormControl>
