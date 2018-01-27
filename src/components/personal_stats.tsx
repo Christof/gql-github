12c17980b1995afff835519600aea78fed553122
christof <christof.sirk@gmail.com>
2018-01-27 11:41:39 +0100

    Start to load stats.

--- src/components/personal_stats.tsx
+++ src/components/personal_stats.tsx
@@ -3,7 +3,7 @@ import {
   DetailedRepositorySelector,
   RepositoriesPerOwner
 } from "./detailed_repository_selector";
-import { Github } from "../github";
+import { Github, GithubAuthorData } from "../github";
 
 interface Props {
   token: string;
@@ -12,21 +12,44 @@ interface Props {
 interface State {
   github: Github;
   repositoriesPerOwner?: RepositoriesPerOwner;
+  author: string;
+  data: GithubAuthorData[];
 }
 
 export class PersonalStats extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
 
-    this.state = { github: new Github(props.token) };
+    const github = new Github(props.token);
+    this.state = { github, author: "", data: [] };
+
+    github.getUser().then(user => this.setState({ author: user.login }));
   }
+
+  async loadData(repositoriesPerOwner: RepositoriesPerOwner) {
+    const data = [] as GithubAuthorData[];
+    for (let [owner, repositories] of repositoriesPerOwner.entries()) {
+      const github = this.state.github.copyFor(owner);
+      await Promise.all(
+        repositories.map(async repo => {
+          const stats = await github.getStats(repo);
+          const authorData = stats.find(
+            authorData => authorData.author.login === this.state.author
+          );
+          data.push(authorData);
+        })
+      );
+    }
+
+    this.setState({ data });
+    console.log(data);
+  }
+
   render() {
     return (
       <DetailedRepositorySelector
         github={this.state.github}
-        onChange={repositoriesPerOwner =>
-          this.setState({ repositoriesPerOwner })
-        }
+        onChange={repositoriesPerOwner => this.loadData(repositoriesPerOwner)}
       />
     );
   }
