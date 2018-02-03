e188665f9476ff86ab671954df1958e45c6844fc
christof <christof.sirk@gmail.com>
2018-02-03 17:27:51 +0100

    Create new OwnerDropdown component.

---
+++ src/components/owner_dropdown.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import { Github } from "../github";
+import { Dropdown } from "./dropdown";
+
+interface Props {
+  github: Github;
+  onSelect: (selected: string) => void;
+}
+
+interface State {
+  owners: string[];
+  iconUrls: string[];
+}
+
+export class OwnerDropdown extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = { owners: [], iconUrls: [] };
+
+    props.github.getOwnersWithAvatar().then(ownersWithAvatar =>
+      this.setState({
+        owners: ownersWithAvatar.map(o => o.login),
+        iconUrls: ownersWithAvatar.map(o => o.avatarUrl)
+      })
+    );
+  }
+
+  render() {
+    return (
+      <Dropdown
+        label="Owner"
+        options={this.state.owners}
+        iconUrls={this.state.iconUrls}
+        onSelect={this.props.onSelect}
+      />
+    );
+  }
+}
