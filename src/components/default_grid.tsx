c54321cf420caf92d816e5deffdb405023538b2a
christof <christof.sirk@gmail.com>
2018-02-10 11:39:13 +0100

    Add small option to DefaultGrid.

--- src/components/default_grid.tsx
+++ src/components/default_grid.tsx
@@ -1,10 +1,15 @@
 import * as React from "react";
 import { Grid } from "material-ui";
 
-export function DefaultGrid(props: { children: React.ReactNode }) {
+export function DefaultGrid(props: {
+  small?: boolean;
+  children: React.ReactNode;
+}) {
+  const sizeProps = props.small ? { xs: 12, md: 10, lg: 8 } : { xs: 12 };
+
   return (
     <Grid container spacing={24} justify="center">
-      <Grid item xs={12}>
+      <Grid item {...sizeProps as any}>
         {props.children}
       </Grid>
     </Grid>
