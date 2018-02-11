f865794fe21a52ef72638d890da3e07e60de495a
christof <christof.sirk@gmail.com>
2018-02-11 15:19:21 +0100

    Add more tests for MenuButton.

--- __tests__/components/menu_button_test.tsx
+++ __tests__/components/menu_button_test.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { MenuButton } from "../../src/components/menu_button";
 import { shallow } from "enzyme";
 import { Button } from "material-ui";
+import { Link } from "react-router-dom";
 
 describe("MenuButton", function() {
   const button = (
@@ -22,5 +23,25 @@ describe("MenuButton", function() {
     history.pushState({}, "route 1", "/route1");
     const wrapper = shallow(button);
     expect(wrapper.prop("color")).toEqual("secondary");
+  });
+
+  it("passes text along as children", function() {
+    const wrapper = shallow(button);
+    expect(wrapper.prop("children")).toEqual("Route1");
+  });
+
+  it("creates a Link component", function() {
+    const wrapper = shallow(button);
+    expect(wrapper.prop("component")()).toEqual(<Link to="/route1" />);
+  });
+
+  it("passes disabled along to Button", function() {
+    const wrapper = shallow(button);
+    expect(wrapper.prop("disabled")).toEqual(false);
+  });
+
+  it("passes className along to Button", function() {
+    const wrapper = shallow(button);
+    expect(wrapper.prop("className")).toEqual("some-class");
   });
 });
