5d4c2430dd19a8b0222eb577d50359274b3c2494
christof <christof.sirk@gmail.com>
2018-02-18 18:42:52 +0100

    Trigger repsitory selection and check for releases in Dropdown.

--- __tests__/components/release_notes_retriever_test.tsx
+++ __tests__/components/release_notes_retriever_test.tsx
@@ -5,11 +5,31 @@ import { Github } from "../../src/github";
 
 jest.mock("../../src/github");
 
-describe("ReleaseNotesRetriever", function() {
-  it("shows selected release note", function() {
-    const github = new Github("token", {} as any);
-    const wrapper = shallow(<ReleaseNotesRetriever github={github} />);
+function waitImmediate() {
+  return new Promise(resolve => {
+    setImmediate(resolve);
+  });
+}
 
-    expect(wrapper.find("RepositorySelector")).toHaveLength(1);
+describe("ReleaseNotesRetriever", function() {
+  it("shows selected release note", async function() {
+    const github = new Github("token", {} as any);
+    let wrapper = shallow(<ReleaseNotesRetriever github={github} />);
+
+    const respositorySelector = wrapper.find("RepositorySelector");
+    expect(respositorySelector).toHaveLength(1);
+
+    github.getReleases = jest.fn(() => [
+      { tagName: "v0.0.1", description: "# v0.0.1" },
+      { tagName: "v0.0.2", description: "# v0.0.2" }
+    ]);
+    (respositorySelector.prop("onRepositorySelect") as any)("myRepo" as any);
+
+    await waitImmediate();
+    wrapper.update();
+
+    const releasesDropdown = wrapper.find("Dropdown");
+    expect(releasesDropdown).toHaveLength(1);
+    expect(releasesDropdown.prop("options")).toEqual(["v0.0.1", "v0.0.2"]);
   });
 });
