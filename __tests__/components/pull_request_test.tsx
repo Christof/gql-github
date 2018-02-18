fe935bb23a81eb571b62792e3b393815bfca9e57
christof <christof.sirk@gmail.com>
2018-02-18 19:45:11 +0100

    Add test for ChangeCategory dropdown in PullRequestComponent.

--- __tests__/components/pull_request_test.tsx
+++ __tests__/components/pull_request_test.tsx
@@ -4,13 +4,13 @@ import { shallow } from "enzyme";
 import { PullRequest, ChangeCategory } from "../../src/pull_request";
 
 describe("PullRequestComponent", function() {
-  it("renders PullRequest text", function() {
-    const pullRequest = new PullRequest(
-      "PR Description",
-      "123",
-      ChangeCategory.Breaking
-    );
+  const pullRequest = new PullRequest(
+    "PR Description",
+    "123",
+    ChangeCategory.Breaking
+  );
 
+  it("renders PullRequest text", function() {
     const wrapper = shallow(
       <PullRequestComponent pullRequest={pullRequest} onChange={() => {}} />
     );
@@ -18,5 +18,23 @@ describe("PullRequestComponent", function() {
     expect(wrapper.find("WithStyles(Typography)").prop("children")).toEqual(
       pullRequest.toText()
     );
+  });
+
+  it("contains a Dropdown to change the ChangeCategory", function() {
+    let changedPullRequest: PullRequest;
+
+    const wrapper = shallow(
+      <PullRequestComponent
+        pullRequest={pullRequest}
+        onChange={pullRequest => {
+          changedPullRequest = pullRequest;
+        }}
+      />
+    );
+
+    wrapper.find("Dropdown").prop("onSelect")("Training" as any);
+
+    expect(changedPullRequest).toBeDefined();
+    expect(changedPullRequest.changeCategory).toEqual(ChangeCategory.Training);
   });
 });
