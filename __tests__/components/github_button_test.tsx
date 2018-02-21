2fbd53a721a6fb93ad318163fcc8c5796ee59f84
christof <christof.sirk@gmail.com>
2018-02-21 19:32:14 +0100

    Get test to fake the location to be localhost.

--- __tests__/components/github_button_test.tsx
+++ __tests__/components/github_button_test.tsx
@@ -2,15 +2,26 @@ import * as React from "react";
 import { GithubButton } from "../../src/components/github_button";
 import { shallow } from "enzyme";
 
+declare const jsdom: any;
+
 describe("GithubButton", function() {
   describe("login", function() {
     describe("localhost", function() {
       it("changes window location to github login page", function() {
+        jsdom.reconfigure({ url: "http://localhost:3000" });
+
         const wrapper = shallow(
           <GithubButton className="some-class" onChangeToken={() => {}} />
         );
 
-        expect(wrapper.find("WithStyles(Button)")).toHaveLength(1);
+        const loginButton = wrapper.find("WithStyles(Button)");
+        expect(loginButton).toHaveLength(1);
+        expect(loginButton.prop("children")[0]).toContain("Login");
+        loginButton.prop("onClick")({} as any);
+
+        expect(window.location.href).toContain(
+          "https://github.com/login/oauth"
+        );
       });
     });
   });
