ac2cdf561eb87be3e8c85feab493050167617cbf
christof <christof.sirk@gmail.com>
2018-02-21 20:07:37 +0100

    Add test for login in production environment using netlify.

--- __tests__/components/github_button_test.tsx
+++ __tests__/components/github_button_test.tsx
@@ -6,7 +6,7 @@ declare const jsdom: any;
 
 describe("GithubButton", function() {
   describe("login", function() {
-    describe("localhost", function() {
+    describe("on localhost", function() {
       it("changes window location to github login page", function() {
         jsdom.reconfigure({ url: "http://localhost:3000" });
         window.location.assign = jest.fn();
@@ -23,6 +23,37 @@ describe("GithubButton", function() {
         expect(window.location.assign).toHaveBeenCalled();
         const newUrl = (window.location.assign as any).mock.calls[0][0];
         expect(newUrl).toContain("https://github.com/login/oauth");
+      });
+    });
+
+    describe("in production", function() {
+      it("changes window location to github login page", function() {
+        jsdom.reconfigure({ url: "http://some-server.com" });
+        const token = "my token";
+        const authenticator = {
+          authenticate: jest.fn((_args: any, callback: any) =>
+            callback(undefined, { token })
+          )
+        };
+
+        let changedToken = "";
+        const wrapper = shallow(
+          <GithubButton
+            className="some-class"
+            onChangeToken={token => {
+              changedToken = token;
+            }}
+            authenticator={authenticator}
+          />
+        );
+
+        const loginButton = wrapper.find("WithStyles(Button)");
+        expect(loginButton).toHaveLength(1);
+        expect(loginButton.prop("children")[0]).toContain("Login");
+        loginButton.prop("onClick")({} as any);
+
+        expect(authenticator.authenticate).toHaveBeenCalled();
+        expect(changedToken).toEqual(token);
       });
     });
   });
