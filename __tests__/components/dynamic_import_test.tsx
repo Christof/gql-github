f296a640bbc2540d5c032f8c043771249ea01d8f
christof <christof.sirk@gmail.com>
2018-03-10 11:11:00 +0100

    Use mount to test all layers and ignore inner workings of DynamicImport.

--- __tests__/components/dynamic_import_test.tsx
+++ __tests__/components/dynamic_import_test.tsx
@@ -1,28 +1,25 @@
 import * as React from "react";
 import { createDynamicImport } from "../../src/components/dynamic_import";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
+import { waitImmediate } from "../helper";
 
 class TestComponent extends React.Component<{ n: number }> {
   render() {
-    return <div />;
+    return <div>Number: {this.props.n}</div>;
   }
 }
 
 describe("createDynamicImport", function() {
-  describe("before resolved", function() {
+  describe("before load is finished", function() {
     it("shows 'Loading!' in h1", function() {
       const Component = createDynamicImport<{ n: number }, TestComponent>(
         () => new Promise<typeof TestComponent>(() => {})
       );
 
-      const wrapper = shallow(<Component n={1} />);
+      const wrapper = mount(<Component n={1} />);
 
-      const dynamicImport = wrapper.find("DynamicImport");
-      expect(dynamicImport).toHaveLength(1);
-      const childrenFunction: any = dynamicImport.prop("children");
-      expect(childrenFunction).toBeDefined();
-      expect(typeof childrenFunction).toBe(typeof Function);
-      expect(childrenFunction()).toEqual(<h1>Loading!</h1>);
+      expect(wrapper.find("h1")).toHaveLength(1);
+      expect(wrapper.find("h1").text()).toEqual("Loading!");
     });
   });
 });
