1f524e888ac62209519254cb129ca226e0012683
christof <christof.sirk@gmail.com>
2018-03-15 20:58:21 +0100

    Add test that checks that DetailedRepositorySelector shows checkboxes for owners.

--- __tests__/components/detailed_repository_selector_test.tsx
+++ __tests__/components/detailed_repository_selector_test.tsx
@@ -19,4 +19,35 @@ describe("DetailedRepositorySelector", function() {
       expect(wrapper.find("WithStyles(LinearProgress)")).toHaveLength(1);
     });
   });
+
+  describe("after data loaded", function() {
+    it("shows checkboxes for all owners ", async function() {
+      const github = new Github("token", {} as any);
+      const owner1 = "owner1";
+      const owner2 = "owner2";
+      github.getOwners = jest.fn(() => Promise.resolve([owner1, owner2]));
+      (github.copyFor as jest.Mock<Github>).mockReturnValue(github);
+      (github.getRepositoryNames as jest.Mock<string[]>).mockReturnValueOnce([
+        "repo1",
+        "repo2"
+      ]);
+      (github.getRepositoryNames as jest.Mock<string[]>).mockReturnValueOnce([
+        "repo3"
+      ]);
+
+      const wrapper = shallow(
+        <DetailedRepositorySelector github={github} onChange={() => {}} />
+      );
+
+      await waitImmediate();
+      wrapper.update();
+
+      expect(wrapper.find("WithStyles(LinearProgress)")).toHaveLength(0);
+
+      const labels = wrapper.find("WithStyles(FormControlLabel)");
+      expect(labels).toHaveLength(2);
+      expect(labels.at(0).prop("label")).toEqual(owner1);
+      expect(labels.at(1).prop("label")).toEqual(owner2);
+    });
+  });
 });
