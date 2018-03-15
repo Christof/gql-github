f3e5929e71b60f11956dfe1982222a0e0e402e59
christof <christof.sirk@gmail.com>
2018-03-15 21:44:49 +0100

    Check that repositories are shown if owner is checked.
    Also check that the selected repositories are returned if accept is
    clicked.

--- __tests__/components/detailed_repository_selector_test.tsx
+++ __tests__/components/detailed_repository_selector_test.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
-import { DetailedRepositorySelector } from "../../src/components/detailed_repository_selector";
+import {
+  DetailedRepositorySelector,
+  RepositoriesPerOwner
+} from "../../src/components/detailed_repository_selector";
 import { shallow, mount } from "enzyme";
 import { waitImmediate } from "../helper";
 import { Github } from "../../src/github";
@@ -21,10 +24,12 @@ describe("DetailedRepositorySelector", function() {
   });
 
   describe("after data loaded", function() {
-    it("shows checkboxes for all owners ", async function() {
-      const github = new Github("token", {} as any);
-      const owner1 = "owner1";
-      const owner2 = "owner2";
+    let github: Github;
+    const owner1 = "owner1";
+    const owner2 = "owner2";
+
+    beforeEach(function() {
+      github = new Github("token", {} as any);
       github.getOwners = jest.fn(() => Promise.resolve([owner1, owner2]));
       (github.copyFor as jest.Mock<Github>).mockReturnValue(github);
       (github.getRepositoryNames as jest.Mock<string[]>).mockReturnValueOnce([
@@ -34,7 +39,9 @@ describe("DetailedRepositorySelector", function() {
       (github.getRepositoryNames as jest.Mock<string[]>).mockReturnValueOnce([
         "repo3"
       ]);
+    });
 
+    it("shows checkboxes for all owners ", async function() {
       const wrapper = shallow(
         <DetailedRepositorySelector github={github} onChange={() => {}} />
       );
@@ -48,6 +55,46 @@ describe("DetailedRepositorySelector", function() {
       expect(labels).toHaveLength(2);
       expect(labels.at(0).prop("label")).toEqual(owner1);
       expect(labels.at(1).prop("label")).toEqual(owner2);
+    });
+
+    it("shows checkboxes for all repositories if owner is checked", async function() {
+      let repositoresPerOwner: RepositoriesPerOwner;
+      const wrapper = shallow(
+        <DetailedRepositorySelector
+          github={github}
+          onChange={data => (repositoresPerOwner = data)}
+        />
+      );
+
+      await waitImmediate();
+      wrapper.update();
+
+      expect(wrapper.find("WithStyles(LinearProgress)")).toHaveLength(0);
+
+      const owner1CheckboxWrapper = shallow(
+        wrapper
+          .find("WithStyles(FormControlLabel)")
+          .at(0)
+          .prop("control")
+      );
+
+      const event = null;
+      const checked = true;
+      (owner1CheckboxWrapper.prop("onChange") as any)(event, checked);
+
+      await waitImmediate();
+      wrapper.update();
+
+      const labels = wrapper.find("WithStyles(FormControlLabel)");
+      expect(labels).toHaveLength(4);
+
+      const acceptButton = wrapper.find("WithStyles(Button)");
+      expect(acceptButton).toHaveLength(1);
+      (acceptButton.prop("onClick") as any)();
+
+      const expected = new Map<string, string[]>();
+      expected.set(owner1, ["repo1", "repo2"]);
+      expect(repositoresPerOwner).toEqual(expected);
     });
   });
 });
