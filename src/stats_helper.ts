b0e9b1ea41af2cdaf99717462ce0b3e1c7adec8f
christof <christof.sirk@gmail.com>
2018-03-17 17:50:29 +0100

    Remove unused getNamesOfOwnRepositories function.

--- src/stats_helper.ts
+++ src/stats_helper.ts
@@ -1,8 +1,3 @@
-export function getNamesOfOwnRepositories(
-  repos: { fork: boolean; name: string }[]
-) {
-  return repos.filter(repo => !repo.fork).map(repo => repo.name);
-}
 export function getCommitsPerAuthorInDateRange(
   data: any[],
   startTime: Date,
