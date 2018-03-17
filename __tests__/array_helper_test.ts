311b7e81b3841b539ccee4bead871535e5d10fa6
christof <christof.sirk@gmail.com>
2018-03-17 17:49:06 +0100

    Add test for unique function.

--- __tests__/array_helper_test.ts
+++ __tests__/array_helper_test.ts
@@ -1,7 +1,13 @@
-import { flatten } from "../src/array_helper";
+import { flatten, unique } from "../src/array_helper";
 
 describe("flatten", function() {
   it("flattens nested arrays", function() {
     expect(flatten([[1], [2, 3], [4, 5]])).toEqual([1, 2, 3, 4, 5]);
+  });
+});
+
+describe("unique", function() {
+  it("removes duplicates in array", function() {
+    expect(unique([1, 1, 2, 4, 3, 3])).toEqual([1, 2, 4, 3]);
   });
 });
