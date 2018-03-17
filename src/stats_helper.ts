be30c122d0ed758d7f8fdea4798a4d1794f4ac0d
christof <christof.sirk@gmail.com>
2018-03-17 21:01:12 +0100

    Remove any types in getCommitsPerAuthorInDateRange function.

--- src/stats_helper.ts
+++ src/stats_helper.ts
@@ -1,19 +1,21 @@
+import { GithubAuthorData } from "./github";
+
 export function getCommitsPerAuthorInDateRange(
-  data: any[],
+  data: GithubAuthorData[],
   startTime: Date,
   endTime = new Date()
 ) {
   const unixStartTime = Math.round(startTime.getTime() / 1000);
   const unixEndTime = Math.round(endTime.getTime() / 1000);
-  return data.reduce((acc, userEntry) => {
-    const weeksInRange = userEntry.weeks.filter(
-      (week: any) => week.w > unixStartTime && week.w <= unixEndTime
-    );
-    const commits = weeksInRange.reduce(
-      (sum: number, week: any) => sum + week.c,
-      0
-    );
-    acc[userEntry.author.login] = commits;
-    return acc;
-  }, {});
+  return data.reduce(
+    (acc, userEntry) => {
+      const weeksInRange = userEntry.weeks.filter(
+        week => week.w > unixStartTime && week.w <= unixEndTime
+      );
+      const commits = weeksInRange.reduce((sum, week) => sum + week.c, 0);
+      acc[userEntry.author.login] = commits;
+      return acc;
+    },
+    {} as { [author: string]: number }
+  );
 }
