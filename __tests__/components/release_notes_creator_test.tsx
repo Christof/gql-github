9266280287c301af4bea60da30041f1ddd77662c
christof <christof.sirk@gmail.com>
2018-03-18 22:17:34 +0100

    Add test for range selection section.

--- __tests__/components/release_notes_creator_test.tsx
+++ __tests__/components/release_notes_creator_test.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { ReleaseNotesCreator } from "../../src/components/release_notes_creator";
-import { shallow } from "enzyme";
+import { shallow, ShallowWrapper } from "enzyme";
 import { waitImmediate } from "../helper";
 import { Github } from "../../src/github";
 
@@ -8,15 +8,15 @@ jest.mock("../../src/github");
 
 describe("ReleaseNotesCreator", function() {
   let github: Github;
+  let wrapper: ShallowWrapper<any, any>;
 
   beforeEach(function() {
     github = new Github("token", {} as any);
+    wrapper = shallow(<ReleaseNotesCreator github={github} />);
   });
 
   describe("before selecting a repository", function() {
     it("shows a RepositorySelector", function() {
-      const wrapper = shallow(<ReleaseNotesCreator github={github} />);
-
       expect(wrapper.find("RepositorySelector")).toHaveLength(1);
     });
 
@@ -24,6 +24,34 @@ describe("ReleaseNotesCreator", function() {
       const wrapper = shallow(<ReleaseNotesCreator github={github} />);
 
       expect(wrapper.find("section")).toHaveLength(3);
+    });
+  });
+
+  describe("after selecting a repository", function() {
+    const tags = [{ name: "v0.0.1" }, { name: "v0.0.2" }];
+
+    beforeEach(async function() {
+      (github.getTags as jest.Mock).mockReturnValue(tags);
+      (wrapper
+        .find("RepositorySelector")
+        .prop("onRepositorySelect") as any)("repo1");
+
+      await waitImmediate();
+      wrapper.update();
+    });
+
+    it("shows the range section", function() {
+      expect(wrapper.find("WithStyles(Typography)")).toHaveLength(1);
+      expect(wrapper.find("WithStyles(Typography)").prop("children")).toEqual(
+        "Range"
+      );
+
+      const dropdowns = wrapper.find("Dropdown");
+      expect(dropdowns).toHaveLength(2);
+      const tagNames = [tags[0].name, tags[1].name];
+
+      expect(dropdowns.at(0).prop("options")).toEqual(tagNames);
+      expect(dropdowns.at(1).prop("options")).toEqual(tagNames);
     });
   });
 });
