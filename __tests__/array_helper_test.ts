e9747fffbb9646c77a8c14ef1a868fd5cf8cbf07
christof <christof.sirk@gmail.com>
2018-03-24 19:27:14 +0100

    Add tests for runningAverage.

--- __tests__/array_helper_test.ts
+++ __tests__/array_helper_test.ts
@@ -1,4 +1,4 @@
-import { flatten, unique } from "../src/array_helper";
+import { flatten, unique, runningAverage } from "../src/array_helper";
 
 describe("flatten", function() {
   it("flattens nested arrays", function() {
@@ -9,5 +9,19 @@ describe("flatten", function() {
 describe("unique", function() {
   it("removes duplicates in array", function() {
     expect(unique([1, 1, 2, 4, 3, 3])).toEqual([1, 2, 4, 3]);
+  });
+});
+
+describe("runningAverage", function() {
+  it("calculates the running average with 1 neighbour before and after", function() {
+    const input = [1, 2, 3, 4, 8];
+
+    expect(runningAverage(input, 1)).toEqual([1.5, 2, 3, 5, 6]);
+  });
+
+  it("calculates the running average with 2 neighbour before and after", function() {
+    const input = [1, 2, 3, 4, 8];
+
+    expect(runningAverage(input, 2)).toEqual([2, 2.5, 3.6, 4.25, 5]);
   });
 });
