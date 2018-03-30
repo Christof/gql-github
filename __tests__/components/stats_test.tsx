6ff618fb146a5720295d0f668e052573e17ecff0
christof <christof.sirk@gmail.com>
2018-03-30 23:20:08 +0200

    Add another test for Stats component.

--- __tests__/components/stats_test.tsx
+++ __tests__/components/stats_test.tsx
@@ -8,16 +8,45 @@ jest.mock("../../src/github");
 
 describe("Stats", function() {
   let github: Github;
+  let wrapper: ShallowWrapper<any, any>;
 
   beforeEach(function() {
     github = new Github("token", {} as any);
+    wrapper = shallow(<Stats github={github} />);
+  });
+
+  it("shows a RepositoriesByOwnerSelector", function() {
+    expect(wrapper.find("RepositoriesByOwnerSelector")).toHaveLength(1);
   });
 
   describe("repository selection", function() {
-    it("shows a RepositoriesByOwnerSelector", function() {
-      const wrapper = shallow(<Stats github={github} />);
+    let repositoriesByOwner: Map<string, string[]>;
+    let resolveForGetRepositoryNames: Function;
 
-      expect(wrapper.find("RepositoriesByOwnerSelector")).toHaveLength(1);
+    beforeEach(async function() {
+      const repositorySelector = wrapper.find("RepositoriesByOwnerSelector");
+
+      (github.getRepositoryNames as jest.Mock).mockReturnValue(
+        new Promise(resolve => {
+          resolveForGetRepositoryNames = resolve;
+        })
+      );
+
+      (repositorySelector.prop(
+        "onLoad"
+      ) as any)({ owner: "owner", includeForks: true });
+
+      await waitImmediate();
+      wrapper.update();
+    });
+
+    it("shows a heading and progress bar", function() {
+      const heading = wrapper.find("WithStyles(Typography)");
+
+      expect(heading).toHaveLength(1);
+      expect(heading.prop("children")).toEqual("Stats");
+
+      expect(wrapper.find("WithStyles(LinearProgress)")).toHaveLength(1);
     });
   });
 });
