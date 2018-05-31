9d253158f9d94d3182d94d6a7eb4a9ec20f81c07
christof <christof.sirk@gmail.com>
2018-05-31 11:06:16 +0200

    Extract methods of OrgStats into functions.

--- src/components/org_stats.tsx
+++ src/components/org_stats.tsx
@@ -14,6 +14,118 @@ import { discardTimeFromDate } from "../utils";
 import { OrgStatsPlots } from "./org_stats_plots";
 import { triggeredAsyncSwitch } from "./triggered_async_switch";
 
+function createTraces(data: GithubAuthorData[][]) {
+  const weeklyCommitsPerAuthor = calculateWeeklyCommits(data);
+
+  const traces = [];
+  for (const authorData of weeklyCommitsPerAuthor.entries()) {
+    const weeks = authorData[1];
+    traces.push({
+      type: "scatter" as any,
+      mode: "lines" as any,
+      name: authorData[0],
+      x: weeks.map(week => new Date(week[0] * 1000)),
+      y: weeks.map(week => week[1])
+    });
+
+    traces.push({
+      type: "scatter" as any,
+      mode: "lines" as any,
+      name: `${authorData[0]} Avg`,
+      x: weeks.map(week => new Date(week[0] * 1000)),
+      y: runningAverage(weeks.map(week => week[1]), 2)
+    });
+  }
+
+  return traces;
+}
+
+async function getPullRequests(github: Github, repositoryNames: string[]) {
+  return flatten<GithubPullRequest>(
+    await Promise.all(
+      repositoryNames.map(repo => github.getPullRequestsWithReviews(repo))
+    )
+  );
+}
+
+function createPullRequestTraces(pullRequests: GithubPullRequest[]) {
+  const pullRequestsByAuthor = groupBy(
+    pullRequest => pullRequest.author,
+    pullRequests
+  );
+
+  return values(
+    mapObjIndexed(
+      (pullRequests: GithubPullRequest[], author: string) => ({
+        type: "scatter" as any,
+        mode: "markers" as any,
+        name: `${author} PRs (${pullRequests.length})`,
+        x: pullRequests.map(pullRequest => new Date(pullRequest.createdAt)),
+        y: pullRequests.map(pullRequest => pullRequest.reviews.length)
+      }),
+      pullRequestsByAuthor
+    )
+  );
+}
+
+function getReviewsByAuthorPerDay(pullRequests: GithubPullRequest[]) {
+  const reviews = flatten<GithubReview>(
+    map(pullRequest => pullRequest.reviews, pullRequests)
+  );
+
+  const reviewsPerDay = map(
+    review => ({
+      ...review,
+      createdAt: discardTimeFromDate(review.createdAt)
+    }),
+    reviews
+  );
+
+  return groupBy(review => review.author, reviewsPerDay);
+}
+
+function createReviewTraces(pullRequests: GithubPullRequest[]) {
+  return values(
+    mapObjIndexed((reviews: GithubReview[], author: string) => {
+      const groupedByDate = groupBy(
+        review => review.createdAt.valueOf().toString(),
+        reviews
+      );
+      return {
+        type: "scatter" as any,
+        mode: "markers" as any,
+        name: `${author} Reviews (${reviews.length})`,
+        x: map(dateString => parseFloat(dateString), keys(groupedByDate)),
+        y: map(reviews => reviews.length, values(groupedByDate))
+      };
+    }, getReviewsByAuthorPerDay(pullRequests))
+  );
+}
+
+async function loadData(
+  github: Github,
+  options: { owner?: string; includeForks: boolean }
+) {
+  const overTimePlotPromise = import("./over_time_plot").then(
+    module => module.OverTimePlot
+  );
+
+  github.owner = options.owner;
+  const repositoryNames = await github.getRepositoryNames(options);
+
+  const data = await github.getStatsForRepositories(repositoryNames);
+
+  const pullRequests = await getPullRequests(github, repositoryNames);
+  const pullRequestsTraces = createPullRequestTraces(pullRequests);
+  const reviewsTraces = createReviewTraces(pullRequests);
+
+  const traces = createTraces(data);
+
+  const OverTimePlot = await overTimePlotPromise;
+
+  return { data, traces, pullRequestsTraces, reviewsTraces, OverTimePlot };
+}
+
 interface Props {
   github: Github;
 }
@@ -21,121 +133,6 @@ interface Props {
 export class OrgStats extends React.Component<Props, {}> {
   constructor(props: Props) {
     super(props);
-  }
-
-  createTraces(data: GithubAuthorData[][]) {
-    const weeklyCommitsPerAuthor = calculateWeeklyCommits(data);
-
-    const traces = [];
-    for (const authorData of weeklyCommitsPerAuthor.entries()) {
-      const weeks = authorData[1];
-      traces.push({
-        type: "scatter" as any,
-        mode: "lines" as any,
-        name: authorData[0],
-        x: weeks.map(week => new Date(week[0] * 1000)),
-        y: weeks.map(week => week[1])
-      });
-
-      traces.push({
-        type: "scatter" as any,
-        mode: "lines" as any,
-        name: `${authorData[0]} Avg`,
-        x: weeks.map(week => new Date(week[0] * 1000)),
-        y: runningAverage(weeks.map(week => week[1]), 2)
-      });
-    }
-
-    return traces;
-  }
-
-  async getPullRequests(repositoryNames: string[]) {
-    return flatten<GithubPullRequest>(
-      await Promise.all(
-        repositoryNames.map(repo =>
-          this.props.github.getPullRequestsWithReviews(repo)
-        )
-      )
-    );
-  }
-
-  createPullRequestTraces(pullRequests: GithubPullRequest[]) {
-    const pullRequestsByAuthor = groupBy(
-      pullRequest => pullRequest.author,
-      pullRequests
-    );
-
-    return values(
-      mapObjIndexed(
-        (pullRequests: GithubPullRequest[], author: string) => ({
-          type: "scatter" as any,
-          mode: "markers" as any,
-          name: `${author} PRs (${pullRequests.length})`,
-          x: pullRequests.map(pullRequest => new Date(pullRequest.createdAt)),
-          y: pullRequests.map(pullRequest => pullRequest.reviews.length)
-        }),
-        pullRequestsByAuthor
-      )
-    );
-  }
-
-  private getReviewsByAuthorPerDay(pullRequests: GithubPullRequest[]) {
-    const reviews = flatten<GithubReview>(
-      map(pullRequest => pullRequest.reviews, pullRequests)
-    );
-
-    const reviewsPerDay = map(
-      review => ({
-        ...review,
-        createdAt: discardTimeFromDate(review.createdAt)
-      }),
-      reviews
-    );
-
-    return groupBy(review => review.author, reviewsPerDay);
-  }
-
-  createReviewTraces(pullRequests: GithubPullRequest[]) {
-    return values(
-      mapObjIndexed((reviews: GithubReview[], author: string) => {
-        const groupedByDate = groupBy(
-          review => review.createdAt.valueOf().toString(),
-          reviews
-        );
-        return {
-          type: "scatter" as any,
-          mode: "markers" as any,
-          name: `${author} Reviews (${reviews.length})`,
-          x: map(dateString => parseFloat(dateString), keys(groupedByDate)),
-          y: map(reviews => reviews.length, values(groupedByDate))
-        };
-      }, this.getReviewsByAuthorPerDay(pullRequests))
-    );
-  }
-
-  async loadData(options: { owner?: string; includeForks: boolean }) {
-    const overTimePlotPromise = import("./over_time_plot").then(
-      module => module.OverTimePlot
-    );
-
-    // this.setState({ startedLoading: true });
-
-    this.props.github.owner = options.owner;
-    const repositoryNames = await this.props.github.getRepositoryNames(options);
-
-    const data = await this.props.github.getStatsForRepositories(
-      repositoryNames
-    );
-
-    const pullRequests = await this.getPullRequests(repositoryNames);
-    const pullRequestsTraces = this.createPullRequestTraces(pullRequests);
-    const reviewsTraces = this.createReviewTraces(pullRequests);
-
-    const traces = this.createTraces(data);
-
-    const OverTimePlot = await overTimePlotPromise;
-
-    return { data, traces, pullRequestsTraces, reviewsTraces, OverTimePlot };
   }
 
   render() {
@@ -150,7 +147,7 @@ export class OrgStats extends React.Component<Props, {}> {
         <Triggered
           github={this.props.github}
           onLoad={(options: { owner?: string; includeForks: boolean }) =>
-            this.loadData(options)
+            loadData(this.props.github, options)
           }
         />
       </DefaultGrid>
