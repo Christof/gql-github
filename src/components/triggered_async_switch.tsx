e5e2941e015818e58f2922ebacc9f64181d02e62
christof <christof.sirk@gmail.com>
2018-07-29 18:20:15 +0200

    Remove now unsed HOC functions.

--- src/components/triggered_async_switch.tsx
+++ src/components/triggered_async_switch.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { LinearProgress } from "material-ui";
 import { zipObj } from "ramda";
 
 interface Props<TriggeredProps> {
@@ -42,83 +41,6 @@ export class TriggeredAsyncSwitch<TriggeredProps> extends React.Component<
 export class TriggeredAsyncSwitchFromLoadType<
   TypeOfLoad extends (...args: any[]) => any
 > extends TriggeredAsyncSwitch<Unpromisify<ReturnType<TypeOfLoad>>> {}
-
-export function triggeredAsyncSwitch<
-  P extends object,
-  PTriggered extends object
->(
-  TriggerComponent: React.ComponentType<P>,
-  triggerCallbackKey: keyof P,
-  TriggeredComponent: React.ComponentType<PTriggered>
-) {
-  type Props = Partial<P> & {
-    onLoad: (...params: any[]) => Promise<PTriggered>;
-  };
-
-  interface State {
-    triggered: boolean;
-    triggeredProps: PTriggered;
-  }
-
-  return class TriggeredAsyncSwitch extends React.Component<Props, State> {
-    constructor(props: Props) {
-      super(props);
-
-      this.state = {
-        triggered: false,
-        triggeredProps: undefined
-      };
-    }
-
-    trigger(...params: any[]) {
-      this.setState({ triggered: true, triggeredProps: undefined });
-      this.props
-        .onLoad(...params)
-        .then(triggeredProps => this.setState({ triggeredProps }));
-    }
-
-    render() {
-      return (
-        <>
-          <TriggerComponent
-            {...this.props}
-            {...{ [triggerCallbackKey]: this.trigger.bind(this) }}
-          />
-          {this.state.triggered && (
-            <TriggeredComponent {...this.state.triggeredProps} />
-          )}
-        </>
-      );
-    }
-  };
-}
-
-export function progressToContentSwitch<P extends object>(
-  ContentComponent: React.ComponentType<P>,
-  Progress: React.ComponentType = LinearProgress
-) {
-  return class ProgressToContentSwitch extends React.Component<P | {}, {}> {
-    render() {
-      return Object.keys(this.props).length === 0 ? (
-        <Progress />
-      ) : (
-        <ContentComponent {...this.props} />
-      );
-    }
-  };
-}
-
-export function container<PContainer extends object, PContainee extends object>(
-  Container: React.ComponentType<PContainer>,
-  containerProps: PContainer,
-  Containee: React.ComponentType<PContainee>
-) {
-  return (props: PContainee) => (
-    <Container {...containerProps}>
-      <Containee {...props} />
-    </Container>
-  );
-}
 
 export type Unpromisify<T> = T extends Promise<infer U> ? U : T;
 type UnpromisifiedObject<T> = { [k in keyof T]: Unpromisify<T[k]> };
