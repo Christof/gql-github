e2f0bb6f54f8265318457a0625a285fb0a1b3725
christof <christof.sirk@gmail.com>
2018-08-03 13:52:55 +0200

    Add PersistentDrawer from examples.

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import classNames from "classnames";
 import { BrowserRouter, Route } from "react-router-dom";
 import { MenuButton } from "./menu_button";
 import { GithubButton } from "./github_button";
@@ -11,11 +12,13 @@ import {
   Toolbar,
   CssBaseline,
   IconButton,
-  Menu
+  Menu,
+  Drawer,
+  Divider
 } from "@material-ui/core";
 import { withStyles, Theme, StyleRules } from "@material-ui/core/styles";
 import { WithStyles } from "@material-ui/core/styles/withStyles";
-import MenuIcon from "@material-ui/icons/Menu";
+import { Menu as MenuIcon, ChevronLeft } from "@material-ui/icons";
 import { Github } from "../github";
 import { createHttpLink } from "apollo-link-http";
 import { setContext } from "apollo-link-context";
@@ -52,6 +55,187 @@ const PersonalStats = createDynamicImport(() =>
 const OrgStats = createDynamicImport(() =>
   import("./org_stats").then(module => module.OrgStats)
 );
+
+const drawerWidth = 240;
+
+const drawerStyles = (theme: Theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  appFrame: {
+    height: 430,
+    zIndex: 1,
+    overflow: "hidden",
+    position: "relative",
+    display: "flex",
+    width: "100%"
+  },
+  appBar: {
+    position: "absolute",
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  "appBarShift-left": {
+    marginLeft: drawerWidth
+  },
+  "appBarShift-right": {
+    marginRight: drawerWidth
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 20
+  },
+  hide: {
+    display: "none"
+  },
+  drawerPaper: {
+    position: "relative",
+    width: drawerWidth
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...theme.mixins.toolbar
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  "content-left": {
+    marginLeft: -drawerWidth
+  },
+  "content-right": {
+    marginRight: -drawerWidth
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  "contentShift-left": {
+    marginLeft: 0
+  },
+  "contentShift-right": {
+    marginRight: 0
+  }
+});
+
+export class CustomDrawer extends React.Component<
+  { classes: any; theme: Theme },
+  { open: boolean }
+> {
+  state = {
+    open: false
+  };
+
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleDrawerClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const props = Object.assign(
+      {
+        disabled: false, //this.state.github === undefined,
+        className: classes.menuButton
+      },
+      this.props
+    );
+    const drawer = (
+      <Drawer
+        variant="persistent"
+        anchor="left"
+        open={this.state.open}
+        classes={{
+          paper: classes.drawerPaper
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={this.handleDrawerClose}>
+            <ChevronLeft />
+          </IconButton>
+        </div>
+        <Divider />
+        <MenuButton to="/stats" text="Stats" {...props} />
+        <MenuButton to="/personal-stats" text="Personal Stats" {...props} />
+        <MenuButton to="/org-stats" text="Org Stats" {...props} />
+        <Divider />
+        <MenuButton
+          to="/retrieve-release-notes"
+          text="Retrieve Release Notes"
+          {...props}
+        />
+        <MenuButton
+          to="/create-release-notes"
+          text="Create Release Notes"
+          {...props}
+        />
+      </Drawer>
+    );
+
+    return (
+      <div className={classes.root}>
+        <div className={classes.appFrame}>
+          <AppBar
+            className={classNames(classes.appBar, {
+              [classes.appBarShift]: open,
+              [classes[`appBarShift-left`]]: open
+            })}
+          >
+            <Toolbar disableGutters={!open}>
+              <IconButton
+                color="inherit"
+                aria-label="Open drawer"
+                onClick={this.handleDrawerOpen}
+                className={classNames(classes.menuButton, open && classes.hide)}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="title" color="inherit" noWrap>
+                Persistent drawer
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          {drawer}
+          <main
+            className={classNames(classes.content, classes[`content-left`], {
+              [classes.contentShift]: open,
+              [classes[`contentShift-left`]]: open
+            })}
+          >
+            <div className={classes.drawerHeader} />
+            <Typography>
+              {"You think water moves fast? You should see ice."}
+            </Typography>
+          </main>
+        </div>
+      </div>
+    );
+  }
+}
+
+const StyledDrawer = withStyles(styles, { withTheme: true })(CustomDrawer);
 
 interface Props {
   fetch: (input: RequestInfo, init?: RequestInit) => Promise<Response>;
@@ -220,6 +404,7 @@ export class RawApp extends React.Component<Props & WithStyles, State> {
         <BrowserRouter>
           <>
             {this.renderAppBar()}
+            <StyledDrawer {...this.props as any} />
             {this.renderContent()}
           </>
         </BrowserRouter>
