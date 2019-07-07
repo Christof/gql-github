8af856b753a4cde1dd7f3b2cf13e2aac44df80ef
christof <christof.sirk@gmail.com>
2019-07-07 15:34:16 +0000

    Add repository selector to Rebaser and show open PRs as debug output.

--- src/components/rebaser.tsx
+++ src/components/rebaser.tsx
@@ -1,6 +1,41 @@
 import * as React from "react";
 import { Github } from "../github";
+import { DefaultGrid } from "./default_grid";
+import { TriggeredAsyncSwitchFromLoadType } from "./triggered_async_switch";
+import { RepositorySelector } from "./repository_selector";
+import { Section } from "./section";
+import { LinearProgress } from "@material-ui/core";
 
-export function Rebaser(_props: { github: Github }) {
-  return <div>Rebaser</div>;
+async function loadPullRequests(repo: string, github: Github) {
+  const pullRequests = await github.getOpenPullRequests(repo);
+
+  return {
+    repo,
+    pullRequests
+  };
+}
+export function Rebaser(props: { github: Github }) {
+  return (
+    <DefaultGrid small>
+      <TriggeredAsyncSwitchFromLoadType<typeof loadPullRequests>
+        renderTrigger={callback => (
+          <RepositorySelector
+            {...props}
+            onRepositorySelect={repository =>
+              callback(loadPullRequests(repository, props.github))
+            }
+          />
+        )}
+        renderTriggered={triggeredProps =>
+          triggeredProps === undefined ? (
+            <Section heading="Pull Requests">
+              <LinearProgress />
+            </Section>
+          ) : (
+            <div>{JSON.stringify(triggeredProps.pullRequests)}</div>
+          )
+        }
+      />
+    </DefaultGrid>
+  );
 }
