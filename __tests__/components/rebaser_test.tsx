d41fb7337c6a18a29f74499e99c24120d93537eb
christof <christof.sirk@gmail.com>
2019-09-01 16:53:42 +0200

    Add some more tests.

--- __tests__/components/rebaser_test.tsx
+++ __tests__/components/rebaser_test.tsx
@@ -4,6 +4,9 @@ import { Github } from "../../src/github";
 import { RepositorySelector } from "../../src/components/repository_selector";
 import { Rebaser, PullRequestSelector } from "../../src/components/rebaser";
 import { waitImmediate } from "../helper";
+import { Dropdown } from "../../src/components/dropdown";
+import { Button } from "@material-ui/core";
+import { act } from "react-dom/test-utils";
 
 jest.mock("../../src/github");
 
@@ -53,6 +56,32 @@ describe("Rebaser", function() {
 
     it("shows a PullRequestSelector", function() {
       expect(wrapper.find(PullRequestSelector)).toHaveLength(1);
+    });
+
+    it("shows the disabled Rebase button", function() {
+      const button = wrapper.find(Button);
+      expect(button).toHaveLength(1);
+      expect(button.prop("disabled")).toBe(true);
+    });
+
+    describe("after a PullRequest has been selected", function() {
+      beforeEach(async function() {
+        act(() => {
+          (wrapper
+            .find(Dropdown)
+            .last()
+            .prop("onSelect") as any)("PR Name");
+        });
+
+        await waitImmediate();
+        wrapper.update();
+      });
+
+      it("shows the Rebase button", function() {
+        const button = wrapper.find(Button);
+        expect(button).toHaveLength(1);
+        expect(button.prop("disabled")).toBe(false);
+      });
     });
   });
 });
