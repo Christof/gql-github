a460027639c8095d7ea67f454ca86345564b0683
christof <christof.sirk@gmail.com>
2022-11-18 16:39:55 +0100

    Update react-markdown to version 6

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -1,10 +1,10 @@
-import * as React from "react";
-import ReactMarkdown from "react-markdown";
+import * as React from 'react';
+import ReactMarkdown from 'react-markdown';
 
 export function Markdown(props: { source: string }) {
   return (
-    <div style={{ fontFamily: "Roboto, Helvetica, Arial, sans-serif" }}>
-      <ReactMarkdown source={props.source} />
+    <div style={{ fontFamily: 'Roboto, Helvetica, Arial, sans-serif' }}>
+      <ReactMarkdown children={props.source} />
     </div>
   );
 }
