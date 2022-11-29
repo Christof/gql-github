99057d37f48eb888aa3eea0f92496200b1806f65
Christof Sirk <christof.sirk@gmail.com>
2022-11-29 09:44:47 +0100

    Add method to parseLinkeHeader
    https://gist.github.com/niallo/3109252

--- src/github.ts
+++ src/github.ts
@@ -307,4 +307,24 @@ export class Github {
       params
     );
   }
+
+  private parseLinkHeader(header: string) {
+    if (header.length === 0) {
+      throw new Error("input must not be of zero length");
+    }
+
+    // Split parts by comma and parse each part into a named link
+    return header.split(/(?!\B"[^"]*),(?![^"]*"\B)/).reduce((links, part) => {
+      const section = part.split(/(?!\B"[^"]*);(?![^"]*"\B)/);
+      if (section.length < 2) {
+        throw new Error("section could not be split on ';'");
+      }
+      const url = section[0].replace(/<(.*)>/, "$1").trim();
+      const name = section[1].replace(/rel="(.*)"/, "$1").trim();
+
+      links[name] = url;
+
+      return links;
+    }, {} as { [index: string]: string });
+  }
 }
