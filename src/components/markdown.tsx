4647077c9dbe9cfa820c42a72bd1b807bff4c6cb
Christof Sirk <christof.sirk@gmail.com>
2022-11-30 11:03:33 +0100

    Revert formatting in markdown to be consistent

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
-import ReactMarkdown from 'react-markdown';
+import * as React from "react";
+import ReactMarkdown from "react-markdown";
 
 export function Markdown(props: { source: string }) {
   return (
-    <div style={{ fontFamily: 'Roboto, Helvetica, Arial, sans-serif' }}>
+    <div style={{ fontFamily: "Roboto, Helvetica, Arial, sans-serif" }}>
       <ReactMarkdown children={props.source} />
     </div>
   );
