1b785432e25778a87876c83d9bb2e7519c79724e
Christof Sirk <christof.sirk@gmail.com>
2023-02-02 13:30:01 +0100

    Update ReactMarkdown and add remarkGfm as plugin
    Now we support GitHub flavored markdown
    (https://github.github.com/gfm/)

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 export function Markdown(props: { source: string }) {
   return (
     <div style={{ fontFamily: "Roboto, Helvetica, Arial, sans-serif" }}>
-      <ReactMarkdown children={props.source} />
+      <ReactMarkdown children={props.source} remarkPlugins={[remarkGfm]} />
     </div>
   );
 }
