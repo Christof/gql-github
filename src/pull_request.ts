3f7cb0522927cf6d26254e0d0b206de2b6e07c83
hannamagdalena <hanna.schweizer@skills-lab.com>
2026-01-26 14:45:00 +0100

    Add some logging

--- src/pull_request.ts
+++ src/pull_request.ts
@@ -16,9 +16,12 @@ export class PullRequest {
       /(?:Merge pull request #(\d+).*?\n\n([\s\S]*)|([\s\S]*?)\s*\(#(\d+)\))/
     );
     const match = commitMessage.match(pullRequestPartsRegex);
-
+    console.log("matches", match);
+    console.log("commitMessage", commitMessage);
     const text = match[2] ?? match[3];
     const id = match[1] ?? match[4];
+    console.log("Parsed PR:", { text, id });
+
     return new PullRequest(text, id, ChangeCategory.Basic);
   }
 
